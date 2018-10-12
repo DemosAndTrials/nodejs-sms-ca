737cc170218b3dc18fdb3f8d7c5fd11006a151cf
Ros Haitovich <matrostik@gmail.com>
2018-10-13 00:26:29 +0300

    ejs

--- src/index.js
+++ src/index.js
@@ -1,19 +1,50 @@
 import express from 'express';
+import path from 'path';
 import middlewaresConfig from './config/middleware';;
-import ApiRoutes from './controllers';
+import Routes from './controllers';
 
 const app = express();
 
 /**
  * MIDDLEWARES
  */
-
 middlewaresConfig(app);
 
 /**
- * API ROUTES
+ * VIEW ENGINE
  */
-app.use('/api', ApiRoutes);
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+/**
+ * ROUTES
+ */
+app.use('/', Routes);
+//app.use('/api', ApiRoutes);
+// errors
+// $ curl http://localhost:3000/notfound
+// $ curl http://localhost:3000/notfound -H "Accept: application/json"
+// $ curl http://localhost:3000/notfound -H "Accept: text/plain"
+app.use(function (req, res, next) {
+    res.status(404);
+    
+    res.format({
+        html: function () {
+            res.render('pages/404', {
+                url: req.url,
+                userData : req.user
+            })
+        },
+        json: function () {
+            res.json({
+                error: 'Not found'
+            })
+        },
+        default: function () {
+            res.type('txt').send('Not found')
+        }
+    })
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
